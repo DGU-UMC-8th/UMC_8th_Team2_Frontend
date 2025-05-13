b5c9ecf7846e8729c8e0fe316fe7943a7e2cbcc1
Doyeon Kim <ehdus022191@dgu.ac.kr>
2025-05-13 20:22:54 +0900

    InitialCommit

---
+++ Seminar_Hackathon/Seminar_Hackathon/Seminar_HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seminar_HackathonApp.swift
+//  Seminar_Hackathon
+//
+//  Created by 김도연 on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Seminar_HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
